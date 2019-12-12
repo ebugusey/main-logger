8440548e9ef9588f15ec7e8489da0c86ea87f9a8
ebugusey <ebugusey@users.noreply.github.com>
2019-12-12 19:42:05 +0400

    feat(db): перенести служебные файлы миграций в отдельный каталог
    Используемый мигратор весьма тупой. Он начинает срать в консоль ошибочными сообщениями
    когда находит в каталоге с миграциями что-то, что он не может обработать.
    
    Если файлы убрать от него подальше, он не будет пытаться их обработать.

--- src/db/migrations/00-baseline.ts
+++ src/db/migrations/00-baseline.ts
@@ -1,10 +1,10 @@
 import { promises as fs } from 'fs'
 import * as path from 'path'
-import { IMigration } from './migration'
+import { IMigration } from './types/migration'
 
 const migration: IMigration = {
     async up(db) {
-        const baseline = path.resolve(__dirname, 'baseline.sql')
+        const baseline = path.resolve(__dirname, 'scripts/baseline.sql')
         const baselineQuery = await fs.readFile(baseline, { encoding: 'utf8' })
 
         await db.sequelize.transaction(async transaction => {
