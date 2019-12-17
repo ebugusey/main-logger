829a35932ac93ab98eaedfde3f7b95d89c0024e5
ebugusey <ebugusey@users.noreply.github.com>
2019-12-17 09:48:11 +0400

    feat(db): заменить миграцию таблицы message_types
    Мы используем TypeScript в проекте. Миграции тоже должны быть написаны на нем,
    но мигратор Sequelize думает по-другому.
    
    К тому же с момента создания миграции изменилась схема именования таблиц и колонок.

---
+++ src/db/migrations/201912170930-create-table-message-types.ts
@@ -0,0 +1,23 @@
+import { IMigration } from './types/migration'
+
+const migration: IMigration = {
+    async up(db, dataType) {
+        await db.createTable('message_types', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: dataType.INTEGER,
+            },
+            description: {
+                allowNull: false,
+                type: dataType.STRING,
+            },
+        })
+    },
+    async down(db) {
+        await db.dropTable('message_types')
+    },
+}
+
+export = migration
